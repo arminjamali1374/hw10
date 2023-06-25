8979a3cd9916c1f84bef93f0c76c6101571941a0
armin <arminjamali73@gmail.com>
2023-06-25 21:57:15 +0330

    In this class, printAnagrams() is called, which prints all the words that can be formed using the letters of the input word. To do this, the number of occurrences of each letter in the input word is counted using a HashMap, and then all possible words are created and printed using the printAnagramsHelper() function.
    The printAnagramsHelper() function constructs and prints all possible words using a HashMap and a character array. At each step, a letter is selected from the HashMap and the number of occurrences of that letter in the HashMap is reduced. Then this letter is placed in the character array and the function is called recursively to place other letters in the character array. At the end of each step, the number of occurrences of the selected letter in the HashMap is returned.

--- src/main/java/org/example/Question1/WordAnagram.java
+++ src/main/java/org/example/Question1/WordAnagram.java
@@ -4,6 +4,35 @@ import java.util.*;
 
 public class WordAnagram {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        printAnagrams(word);
+    }
+
+    public static void printAnagrams(String word) {
+        HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+        for (char c : word.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        printAnagramsHelper(charCount, new char[word.length()], 0, word.length());
+    }
+
+    public static void printAnagramsHelper(HashMap<Character, Integer> charCount,
+                                           char[] currentWord, int index, int length) {
+        if (index == length) {
+            System.out.println(new String(currentWord));
+            return;
+        }
+        for (char c : charCount.keySet()) {
+            int count = charCount.get(c);
+            if (count > 0) {
+                charCount.put(c, count - 1);
+                currentWord[index] = c;
+                printAnagramsHelper(charCount, currentWord, index + 1, length);
+                charCount.put(c, count);
+            }
+        }
 
     }
 }
