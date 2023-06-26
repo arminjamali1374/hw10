d4aa0b6dda8bda237437868440847fa4930c5da0
armin <arminjamali73@gmail.com>
2023-06-26 14:09:14 +0330

    Create hashmap class and create hashmap and insert

---
+++ src/main/java/org/example/Question4/MyHashMap.java
@@ -0,0 +1,16 @@
+package org.example.Question4;
+
+import java.util.HashMap;
+
+public class MyHashMap {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> myHashMap = new HashMap<>();
+
+        myHashMap.put("a", 1);
+        myHashMap.put("b", 2);
+        myHashMap.put("c", 3);
+
+    }
+}
+
